4b810c2ab0e66b7f74d26b58b8fbed98fd821bea
ivan <>
2023-07-04 00:47:08 +0200

    new version with xor/and/left-right-shift

--- vhdl/encryptor.vhd
+++ vhdl/encryptor.vhd
@@ -2,9 +2,9 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date: 06/12/2023 11:58:52 PM
+-- Create Date: 07/02/2023 01:49:14 PM
 -- Design Name: 
--- Module Name: xtea - Behavioral
+-- Module Name: encryptor - Behavioral
 -- Project Name: 
 -- Target Devices: 
 -- Tool Versions: 
@@ -21,13 +21,10 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.std_logic_unsigned.all;
--- lib for bitshifting
+use IEEE.std_logic_unsigned;
+use IEEE.std_logic_unsigned;
 use ieee.numeric_std.all;
---use ieee.std_logic_1164.all;
-use IEEE.STD_LOGIC_1164.ALL;
-
-
+use ieee.std_logic_textio;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -38,47 +35,40 @@ use IEEE.STD_LOGIC_1164.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity xtea is
-    Port ( 
-    clk, reset,start: in STD_LOGIC;
-        
-    input_hexOne : in STD_LOGIC_VECTOR(31 downto 0);
-    input_hexTwo : in STD_LOGIC_VECTOR(31 downto 0);
-    input_numOfCycles : in STD_LOGIC_VECTOR(7 downto 0);
-    input_keyOne: in STD_LOGIC_VECTOR(31 downto 0);
-    input_keyTwo: in STD_LOGIC_VECTOR(31 downto 0);
-    input_keyThree: in STD_LOGIC_VECTOR(31 downto 0);
-    output_hexOne: out std_logic_vector(31 downto 0);
-    output_hextwo: out std_logic_vector(31 downto 0)
+entity encryptor is
+    Port(
+        clk, reset,start: in STD_LOGIC
     );
-end xtea;
+--  Port ( );
 
-architecture Behavioral of xtea is
-    --
-    --signal first_string: unsigned(0 to 8);
-    signal input_string_one: STD_LOGIC_VECTOR(31 downto 0);
-    --signal second_string: unsigned(0 to 8);
-    signal input_string_two: STD_LOGIC_VECTOR(31 downto 0);
-    --TODO declare delta from wikipedia example
-    signal delta: STD_LOGIC_VECTOR( 31 downto 0 ):= x"9E3779B9";
-    --signal Data_Byte : std_logic_vector( 7 downto 0) := x"41";
-    signal A_temp: STD_LOGIC_VECTOR (31 downto 0);
-    signal B_temp: STD_LOGIC_VECTOR (31 downto 0);
-               
-    signal A_encipher_shifting: std_logic_vector(31 downto 0);
-    signal B_decipher_shifting: std_logic_vector(31 downto 0);
-    signal A_sum: std_logic_vector(31 downto 0);
-    signal B_sum: std_logic_vector(31 downto 0);
-    signal result_one: std_logic_vector(7 downto 0);
-    
-   
-    type t_states is (idle,clockOne,clockTwo,clockThree);
-    signal s_states : t_states;
+end encryptor;
 
-    
+architecture Behavioral of encryptor is
+
+        signal v0: std_logic_vector(31 downto 0)        :="11100000000001100000000000000011";
+        signal v1: std_logic_vector(31 downto 0)        :="11100000000001100000000000000011";
+        signal delta: std_logic_vector(31 downto 0)     :="11100000000001100000000000000011";
+        signal temp_m1: std_logic_vector(31 downto 0)   :="00000000000000000000000000000000";
+        signal temp_m2: std_logic_vector(31 downto 0)   :="00000000000000000000000000000000";
+        signal temp_m3: std_logic_vector(31 downto 0)   :="00000000000000000000000000000000";
+        signal temp_m4: std_logic_vector(31 downto 0)   :="00000000000000000000000000000000";
+        signal temp_m5: std_logic_vector(31 downto 0)   :="00000000000000000000000000000000";
+        signal temp_m6: std_logic_vector(31 downto 0)   :="00000000000000000000000000000000";
+        signal res: std_logic_vector(31 downto 0)       :="00000000000000000000000000000000";
+        signal sum: std_logic_vector(31 downto 0)       :="10100000000000000000000000000101";
+        signal keyOne: std_logic_vector(31 downto 0)    :="00000000000000000000000000000000";
+        signal testOne: std_logic_vector(7 downto 0)    :="00000001";
+        signal testTwo: std_logic_vector(7 downto 0)    :="00000011";
+        signal testSum: std_logic_vector(7 downto 0)    :="00000011";
+        
+           
+        type t_states is (idle,clockOne,clockTwo,clockThree);
+        signal s_states : t_states;
+        
     begin
-        input_string_one <= (input_hexOne);
-        input_string_two <= (input_hexOne);
+    -----------------------
+    
+    
     process(clk,reset)
     begin
         if reset = '1' then 
@@ -92,44 +82,65 @@ architecture Behavioral of xtea is
                 else
                     s_states <= clockOne;
             end if;  
-            -- first cycle
+            -- encryptor cycle
             when clockOne => 
-                A_encipher_shifting <= std_logic_vector (unsigned (input_string_one) sll 4) ; 
-                B_decipher_shifting <= std_logic_vector (unsigned (input_string_two) srl 5) ; 
-                A_sum <= std_logic_vector (signed (A_encipher_shifting)) xor std_logic_vector (signed (B_decipher_shifting));
-               --A_sum <= std_logic_vector (unsigned (input_string_one) srl 5);
+                -- clock one
+                temp_m1 <= v1( v1'high-4 downto 0) & "0000"  ;
+                temp_m2 <=  "00000"  & v1( v1'high downto 5) ;
+                temp_m3 <= temp_m1  xor temp_m2;
+                -- TODO add two signals
+                --      --res <= temp_m3 + v1;
+                temp_m4 <= std_logic_vector(unsigned(temp_m3) + unsigned(v1));
+                --testSum <= std_logic_vector(unsigned(testOne) + unsigned(testTwo));
+                --right side miro
+                temp_m5 <= sum AND "00000000000000000000000000000011";
+                --TODO index keyOne[temp_m4]
+                -- temp_m6 <= std_logic_vector(unsigned(keyOne[temp_m4]) + unsigned(sum));
                 
-                -- TODO 
-                B_temp <= B_sum & "0011";
+                --
+                -- Data: in std_logic_vector(7 downto 0);
+                -- signal counter : std_logic_vector(3 downto 0);
+                -- output <= Data(to_integer(unsigned(counter))); 
                 s_states <= clockTwo;
+            when clockTwo =>
             
-            when clockTwo => 
-                A_sum <= std_logic_vector (signed (A_sum)) xor std_logic_vector (signed (B_sum));
-                B_sum <= B_sum + delta;
-
-                A_temp <= input_hexOne + B_sum;
-
-                A_temp <=  std_logic_vector (unsigned (A_temp) sll 4);
-                B_temp <=  std_logic_vector (unsigned (B_temp) srl 5);
-                A_sum <= std_logic_vector (signed (A_temp)) xor std_logic_vector (signed (B_temp));
-
-                
-                B_sum <=  std_logic_vector (unsigned (B_sum) srl 11);
-                B_temp <= B_sum & "0011";
-                
-
-                
-                s_states <= clockThree;
+                -- clock two
+                -- left side
+                temp_m1 <= temp_m4 xor v1;
+                v0 <= std_logic_vector(unsigned(temp_m1) + unsigned(v0));
+                temp_m2 <= v0(v0'high-4 downto 0) & "0000";
+                temp_m3 <= "00000"& v0(v0'high downto 5);
+                temp_m4 <= temp_m2 xor temp_m3;
             
+                -- right side
+                sum <= std_logic_vector(unsigned(sum) + unsigned(delta));
+                temp_m5 <= sum(sum'high downto 11) & "00000000000";
+                temp_m6 <= temp_m5 AND "00000000000000000000000000000011";
+                --TODO index keyOne[temp_m4]
+                -- temp_m3 <= std_logic_vector(unsigned(keyOne[temp_m4]) + unsigned(sum));
+                        
+                s_states <= clockThree;        
             when clockThree =>
-                --A_sum <= A_sum + 
-                B_sum <= B_temp + B_sum;
-                
-                s_states <= idle;
-            
-            end case;       
+                    
+                --clock three
+                temp_m1 <= std_logic_vector(unsigned(temp_m6) + unsigned(v0));
+                -- temp_m2 <= std_logic_vector(unsigned(temp_m4) + unsigned(sum));
+                temp_m3 <= temp_m1 xor temp_m2 ; 
+                v1 <= std_logic_vector(unsigned(temp_m6) + unsigned(v1));
+                s_states <= idle; 
+        end case;       
         --elsif idle'event and idle='1' then
-        end if;
+     end if;
      --   if rising_edge (clk) then
     end process;
+  --Test commands 
+   --report "mySignal = " & to_string(myVar);
+  --report "mySignal = " & to_string(mySignal);
+   -- myVar <= myVar( myVar'high-1 downto 0) & "0" ;
+    --myVar <= myVar( myVar'high-1 downto 0) & "0" ;
+    --myVar2 <= "1" & myVar2( myVar2'high downto 1)  ;
+    --myVar2 <= "1" & myVar2( myVar2'high downto 1)  ;
+    --myXor <= myVar xor myVar2;
+  
 end Behavioral;
+
